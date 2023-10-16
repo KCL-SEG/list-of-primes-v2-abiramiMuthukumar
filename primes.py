36bfae848f8cccd7473611449b7ad5126349fbcd
Abirami Muthukumar <abirami.muthukumar@kcl.ac.uk>
2023-10-16 18:44:09 +0100

    Add ValueError exception

--- primes.py
+++ primes.py
@@ -2,5 +2,24 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if(number_of_primes > 0):
+        list = []
+
+        current_number = 2
+
+        while(len(list) < number_of_primes):
+            prime = True
+            for i in range(2,current_number):
+                if current_number%i == 0:
+                    prime=False
+                    break
+                i+=1
+
+            if prime == True:
+                list.append(current_number)
+
+            current_number+=1
+
+        return list
+    else:
+        raise ValueError
